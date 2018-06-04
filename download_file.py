37bac14d7b361c5c7e6c90e045f9e5c9c8c3ed6d
苏晟 <neday@bk.ru>
2018-06-04 17:17:10 +0800

    FIRST COMMIT!

--- download_file.py
+++ download_file.py
@@ -2,17 +2,17 @@ import os
 import urllib3
 
 
-def download_file(img_url, file_name, file_path='cache/faces'):
+def download_file(img_url, file_name, dir_path='cache/faces'):
     # 保存文件到磁盘文件夹 file_path 中，默认为当前脚本运行目录下的 cache/faces 文件夹
     try:
-        if not os.path.exists(file_path):
-            print('文件夹', file_path, '不存在，重新建立')
-            os.makedirs(file_path)
+        if not os.path.exists(dir_path):
+            print('文件夹', dir_path, '不存在，重新建立')
+            os.makedirs(dir_path)
         # 下载图片，并保存到文件夹中
         http = urllib3.PoolManager()
         print(img_url)
         response = http.request('GET', img_url)
-        save_path = '{}{}{}'.format(file_path, os.sep, file_name)
+        save_path = '{}{}{}'.format(dir_path, os.sep, file_name)
         with open(save_path, 'wb') as f:
             f.write(response.data)
         response.release_conn()
--- face_recognition_server_knn.py
+++ face_recognition_server_knn.py
@@ -21,15 +21,15 @@ class FaceRecognition(face_recognition_pb2_grpc.FaceRecognitionServicer):
         return face_recognition_pb2.GetFaceCountReply(count=count)
 
     def IsMatchFace(self, request, context):
-        file_path = 'cache/temps/' + request.faceToken
-        save_path = download_pic(request.prefixCosUrl, request.fileName, file_path=file_path)
-        is_match_face = get_is_match_face_by_trained(save_path, request.faceToken)
+        save_temp_dir_path = 'cache/temps/' + request.faceToken
+        save_temp_file_path = download_pic(request.prefixCosUrl, request.fileName, dir_path=save_temp_dir_path)
+        is_match_face = get_is_match_face_by_trained(save_temp_file_path, request.faceToken)
         return face_recognition_pb2.IsMatchFaceReply(isMatchFace=is_match_face)
 
 
-def download_pic(prefix_cos_url, file_name, file_path):
-    save_path = download_file(prefix_cos_url + file_name, file_name, file_path)
-    return save_path
+def download_pic(prefix_cos_url, file_name, dir_path):
+    file_path = download_file(prefix_cos_url + file_name, file_name, dir_path)
+    return file_path
 
 
 def found_face_count(image):
@@ -47,46 +47,44 @@ def get_face_encodings(image):
 
 
 def get_face_count_and_encodings(prefix_cos_url, file_name, face_token):
-    file_path = 'cache/faces/' + face_token
-    if os.path.exists(file_path):
+    save_face_dir_path = 'cache/faces/' + face_token
+    if os.path.exists(save_face_dir_path):
         # error
         return -1
-    save_path = download_pic(prefix_cos_url, file_name, file_path)
+    save_face_file_path = download_pic(prefix_cos_url, file_name, save_face_dir_path)
     # Load the uploaded image file
-    image = face_recognition.load_image_file(save_path)
-    count = found_face_count(image)
+    face_image = face_recognition.load_image_file(save_face_file_path)
+    count = found_face_count(face_image)
     if count == 1:
         # STEP 1: Train the KNN classifier and save it to disk
         # Once the model is trained and saved, you can skip this step next time.
         print("Training KNN classifier...")
-        face_token_path = 'cache/all_face_token.clf'
-        train('cache/faces', model_save_path=face_token_path, n_neighbors=2)
+        face_token_file_path = 'cache/all_face_token.clf'
+        train('cache/faces', model_save_path=face_token_file_path, n_neighbors=2)
         print("Training complete!")
         return count
     else:
-        os.removedirs(file_path)
+        os.removedirs(save_face_dir_path)
         return count
 
 
-def get_is_match_face_by_trained(save_path, face_token):
+def get_is_match_face_by_trained(save_temp_file_path, face_token):
     # STEP 2: Using the trained classifier, make predictions for unknown images
-
-    print("Looking for faces in {}".format(save_path))
+    print("Looking for faces in {}".format(save_temp_file_path))
 
     # Find all people in the image using a trained classifier model
     # Note: You can pass in either a classifier file name or a classifier model instance
+    face_token_file_path = 'cache/all_face_token.clf'
 
-    face_token_path = 'cache/all_face_token.clf'
-
-    if not os.path.exists(save_path):
+    if not os.path.exists(save_temp_file_path):
         return False
 
-    if not os.path.exists(face_token_path):
-        os.remove(save_path)
+    if not os.path.exists(face_token_file_path):
+        os.remove(save_temp_file_path)
         return False
 
-    predictions = predict(save_path, model_path=face_token_path)
-    os.remove(save_path)
+    predictions = predict(save_temp_file_path, model_path=face_token_file_path)
+    os.remove(save_temp_file_path)
 
     # Print results on the console
     for name, (top, right, bottom, left) in predictions:
